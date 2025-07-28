ab251027803baf7fc344cfcc522aa74973eb086d
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2025-07-28 20:47:27 +0200

    feat: part selector improvement

--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,7 @@ export const typecast = (value) => {
 }
 
 export const partSelector = (name, selector, nodeName) => (
-  `[data-${nodeName}-part="${selector?.length > 0 ? selector : name.slice(1)}"]`
+  `[data-${nodeName}-part~="${selector?.length > 0 ? selector : name.slice(1)}"]`
 )
 
 export const querySelector = (host, selector) =>
